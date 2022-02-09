d2339306d3288803bf8c3ddf7a65c2847366bdc6
ryudongjae <rdj10149@gmail.com>
2022-02-09 21:32:01 +0900

    SQL

--- Database/basic.sql
+++ Database/basic.sql
@@ -17,3 +17,17 @@ select * from 테이블명 where name='아무개';
 //null값
 select * from 테이블명 where 조건1 is null;
 
+//검색조건
+select * from 테이블명 where a<>0 and b<>0; //a,b열이 모두 0이 아닌 행 검색
+
+select * from 테이블명 where a<>0 or b<>0; // a열이 0이 아니거나 b열이 0이 아닌 행 검색
+
+select * from 테이블명 where not(a<>0 or b<>0);
+
+//패턴 매칭에 의한 검색
+select * from 테이블명 where text like 'sql%'; //전방일치
+select * from 테이블명 where text like '%sql%'; //중간일치
+
+select * from 테이블명 where text LIKE '%\%%'; //text 열에 '%'(메타문자)를 포함하는 행을 검색
+
+select * from 테이블명 where text LIKE 'is''s';//text 열에 is,s를 포함하는 행을 검색
